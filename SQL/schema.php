8f16c8ca2e45158249d6255d573b7dcb10fb776a
Valentin T <Eywek@users.noreply.github.com>
2018-04-29 21:51:15 +0200

    Update schema.php

--- SQL/schema.php
+++ SQL/schema.php
@@ -17,7 +17,7 @@ class StatusAppSchema extends CakeSchema {
         public $servers = [
             'status-iconurl' => array('type' => 'string', 'null' => false, 'default' => 'https://pixelads.fr/img/cover/1497104509.png', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 						'status-motd' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-						'status-isShowIp' => array('type' => 'integer', 'null' => false, 'default' => '1', 'unsigned' => fals),
+						'status-isShowIp' => array('type' => 'integer', 'null' => false, 'default' => '1', 'unsigned' => false),
 						'status-isShowCount' => array('type' => 'integer', 'null' => false, 'default' => '1', 'unsigned' => false),
 						'status-isShowStatus' => array('type' => 'integer', 'null' => false, 'default' => '1', 'unsigned' => false)
         ];
